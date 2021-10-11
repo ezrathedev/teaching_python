a1a6470c29660ccba674f73f626c25a1675d1f7a
Ezra Hampton <ezra.hamptoniii@gmail.com>
2021-10-11 17:44:59 -0400

    working on lesson for cyber student

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# checking your system to make sure python is installed 
+
+
+# creating variables and diffrent data types.
+number = 2 #int
+float = 2.0 # float
+boolean = True or False #bool
+string = 'string' #string
+
+
+"""creating comments  """
+
+#single line comments look like this!
+#single line comment
+
+
+#multi line comments look like this!
+
+"""this is a mulit line comment """
